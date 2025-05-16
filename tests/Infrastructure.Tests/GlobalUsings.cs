46eb7bcf61067c0ecd67124c8e9b5d4e96073652
Rodrigo Figueira <rodrigoservice@gmail.com>
2025-05-16 17:42:01 -0300

    Inclusão Projeto de Teste para Infrastructure

---
+++ tests/Infrastructure.Tests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+﻿global using Application.Abstractions.Repositories;
+global using Infrastructure.Extensions;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
---
+++ tests/Infrastructure.Tests/UrlRepositoryDapperTests.cs
@@ -0,0 +1,45 @@
+﻿using Domain.Entities;
+
+namespace Infrastructure.Tests;
+
+public class UrlRepositoryDapperTests
+{
+    private readonly IUrlRepository _repository;
+
+    public UrlRepositoryDapperTests()
+    {
+        const string dbFile = "test.db";
+        if (File.Exists(dbFile))
+            File.Delete(dbFile);
+
+        var host = Host.CreateDefaultBuilder()
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                config.AddJsonFile("appsettings.json");
+            })
+            .ConfigureServices((context, services) =>
+            {
+                services.AddPersistence(context.Configuration);
+            })
+            .Build();
+
+        host.ApplyMigrations();
+
+        var scope = host.Services.CreateScope();
+        _repository = scope.ServiceProvider.GetRequiredService<IUrlRepository>();
+    }
+
+    [Fact]
+    public async Task DeveInserirUrlComSucesso()
+    {
+        //arrange
+        var entity = new UrlEntity(0, "abc123", "https://example.com");
+
+        //act
+        var result = await _repository.Post(entity);
+
+        //assert
+        Assert.NotEqual(0, result.Id);
+        Assert.Equal("abc123", result.Alias);
+    }
+}
